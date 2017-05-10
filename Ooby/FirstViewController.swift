88a798d007a65e2b544a41901b3d6ec23cd9b8d2
Tanzil Ansari <ansari.tanzil123@gmail.com>
2017-05-10 17:21:12 -0400

    Initial Commit

---
+++ Ooby/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Ooby
+//
+//  Created by Tanzil Ansari on 5/10/17.
+//  Copyright © 2017 tansari. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
